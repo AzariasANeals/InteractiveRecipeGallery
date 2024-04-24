d25c729320cb8e63d068ffcb29e3aeef7ab56434
AzariasANeals <165864620+AzariasANeals@users.noreply.github.com>
2024-04-24 14:29:12 -0700

    Current Code -- Update 1

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Image, ScrollView,  Alert, TextInput, ImageBackground} from 'react-native';
+import {images, names, nameList} from './imageList.js';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+      <Image source={images.id=1} styles={{width:30, height:30}}/>
+      <StatusBar style="auto" />      
+
+      </View>    
   );
 }
-
+//<Image source={imageOne} style={styles.imageStyle}></Image>
+//<Image source={imageTwo} style={styles.imageStyle}></Image>
+const imageOne = require('./assets/pizza.jpg')
+const imageTwo = require('./assets/Classic-Lasagna.jpg')
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black'
+  },
+  text:{
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  imageStyle:{
+    width:300,
+    height:200
+  },
+  scrollView:{
+    marginHorizontal:50,
+    },
+
+  scrollText:{
+    marginTop:20,
+    marginLeft:15,
+    marginRight:15,
+    fontFamily:'Optima',
+    fontWeight:'500',
+    color:'#00542D',
+    fontSize: 30,
   },
 });
---
+++ imageList.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Pressable, Image, ScrollView,  Alert, TextInput} from 'react-native';
+// imageList.js
+export const images = [
+    {
+      id: 1,
+      image:require('./assets/pizza.jpg'),
+      description: 'This is a picture of a Pizza!'
+    },
+
+    {
+      id: 2,
+      image:require('./assets/Classic-Lasagna.jpg'),
+      description: 'This is a picture of a Lasagna!'
+    },
+    // Add more images as needed
+  ];
+
+  
+  export const names = ['Alice', 'Bob', 'Charlie'];
+  export const nameList =(
+    <View>{names.map(name => <Text key={name}> {name} </Text>)}</View>
+  );
+
+
+  // const gallery = images.filter()
+
+//const imageOne = require('./assets/pizza.jpg')
+// const imageTwo = require('./assets/Classic-Lasagna.jpg')
